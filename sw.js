edb5bd67582c3c0805ef495fe3de30eed990347d
brickjawn <brickjawnt@gmail.com>
2025-05-29 19:07:25 -0400

    initial commit with PWA game

---
+++ sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'pip-inferno-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.js',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+  './pip_icon_192.png',
+  './pip_icon_512.png',
+  './manifest.json'
+];
+
+// Install event: Cache core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request).then(
+          networkResponse => {
+            // Check if we received a valid response
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            // Clone the response
+            const responseToCache = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
